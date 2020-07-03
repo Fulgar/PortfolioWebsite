904d49e2742541e04f2491ff760de5e58344ef9b
Jason <jdawgerj5@gmail.com>
2020-07-03 05:22:12 -0400

    Initial commit

---
+++ src/main/java/ApplicationController.java
@@ -0,0 +1,21 @@
+import endpoints.TestEndpoint;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ApplicationController extends Application
+{
+	private Set<Object> singletons = new HashSet<>();
+
+	// Adds endpoints
+	public ApplicationController() {
+		singletons.add(new TestEndpoint());
+	}
+
+	@Override
+	public Set<Object> getSingletons() {
+		return singletons;
+	}
+}
