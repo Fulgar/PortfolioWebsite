3055d52dac1ee59f327e9f3d3866686f8873a1e4
Jason <jdawgerj5@gmail.com>
2020-07-06 21:08:59 -0400

    Added basic "CR-D" DAO for junction tables

---
+++ src/main/java/dao/Project_ContributorDAO.java
@@ -0,0 +1,134 @@
+package dao;
+
+import dto.Project_ContributorDTO;
+import rowmap.Project_ContributorMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Project_ContributorDAO
+{
+	// Retrieves all project_Contributors from database
+	public List<Project_ContributorDTO> getAllProject_Contributors() throws SQLException
+	{
+		// List of all project_Contributors in DTO form
+		List<Project_ContributorDTO> allProject_Contributors = new ArrayList<>();
+
+		// SQL Query statement
+		String QUERY = "SELECT * FROM PROJECTS_CONTRIBUTORS";
+
+		ResultSet rs = DatabaseWrapper.getQueryResult(QUERY);
+
+		Project_ContributorMapper mapper = new Project_ContributorMapper();
+
+		// Iterate through results and map database entries into DTO objects
+		while(rs.next())
+		{
+			Project_ContributorDTO newProject_Contributor = mapper.rowMap(rs);
+			allProject_Contributors.add(newProject_Contributor);
+		}
+
+		return allProject_Contributors;
+	}
+
+	// Retrieves list of Project_Contributors via ProjectID in DTO form
+	public List<Project_ContributorDTO> getProject_ContributorsByProjectID(int projectID) throws SQLException
+	{
+		// Result
+		List<Project_ContributorDTO> allProject_Contributors = new ArrayList<>();
+
+		// SQL Query statement
+		String QUERY = "SELECT * FROM PROJECTS_CONTRIBUTORS WHERE ProjectID=" + projectID;
+
+		ResultSet rs = DatabaseWrapper.getQueryResult(QUERY);
+
+		Project_ContributorMapper mapper = new Project_ContributorMapper();
+
+		// Iterate through results and map database entries into DTO objects
+		while(rs.next())
+		{
+			Project_ContributorDTO newProject_Contributor = mapper.rowMap(rs);
+			allProject_Contributors.add(newProject_Contributor);
+		}
+
+		return allProject_Contributors;
+	}
+
+	// Retrieves list of Project_Contributors via ContributorID in DTO form
+	public List<Project_ContributorDTO> getProject_ContributorsByContributorID(int contributorID) throws SQLException
+	{
+		// Result
+		List<Project_ContributorDTO> allProject_Contributors = new ArrayList<>();
+
+		// SQL Query statement
+		String QUERY = "SELECT * FROM PROJECTS_CONTRIBUTORS WHERE ContributorID=" + contributorID;
+
+		ResultSet rs = DatabaseWrapper.getQueryResult(QUERY);
+
+		Project_ContributorMapper mapper = new Project_ContributorMapper();
+
+		// Iterate through results and map database entries into DTO objects
+		while(rs.next())
+		{
+			Project_ContributorDTO newProject_Contributor = mapper.rowMap(rs);
+			allProject_Contributors.add(newProject_Contributor);
+		}
+
+		return allProject_Contributors;
+	}
+
+	// Retrieves list of Project_Contributors via ProjectID AND ContributorID in DTO form
+	public List<Project_ContributorDTO> getProject_ContributorByBothID(int projectID, int contributorID) throws SQLException
+	{
+		// Result
+		List<Project_ContributorDTO> allProject_Contributors = new ArrayList<>();
+
+		// SQL Query statement
+		String QUERY = "SELECT * FROM PROJECTS_CONTRIBUTORS WHERE ProjectID=" + projectID + " AND ContributorID=" + contributorID;
+
+		ResultSet rs = DatabaseWrapper.getQueryResult(QUERY);
+
+		Project_ContributorMapper mapper = new Project_ContributorMapper();
+
+		// Iterate through results and map database entries into DTO objects
+		while(rs.next())
+		{
+			Project_ContributorDTO newProject_Contributor = mapper.rowMap(rs);
+			allProject_Contributors.add(newProject_Contributor);
+		}
+
+		return allProject_Contributors;
+	}
+
+	// Inserts project_Contributor into database table
+	public Project_ContributorDTO createProject_Contributor (Project_ContributorDTO project_Contributor) throws SQLException
+	{
+
+		// SQL Query statement
+		String QUERY = "INSERT INTO PROJECTS_CONTRIBUTORS VALUES (";
+		QUERY += project_Contributor.getProjectID() + ", ";
+		QUERY += project_Contributor.getContributorID() + ")";
+
+		// Executes statement
+		ResultSet rs = DatabaseWrapper.getQueryResult(QUERY);
+
+		return project_Contributor;
+	}
+
+	// No Update method needed for junction/association table
+
+	// Deletes project_Contributor
+	public void deleteProject_Contributor (Project_ContributorDTO project_Contributor) throws SQLException
+	{
+		int projectID = project_Contributor.getProjectID();
+		int contributorID = project_Contributor.getContributorID();
+
+		// SQL Query statement
+		String QUERY = "DELETE FROM PROJECTS_CONTRIBUTORS WHERE ProjectID=" + projectID + " AND ContributorID=" + contributorID;
+
+		// Executes statement
+		ResultSet rs = DatabaseWrapper.getQueryResult(QUERY);
+	}
+}
---
+++ src/main/java/dao/Project_TechnologyTagDAO.java
@@ -0,0 +1,134 @@
+package dao;
+
+import dto.Project_TechnologyTagDTO;
+import rowmap.Project_TechnologyTagMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Project_TechnologyTagDAO
+{
+	// Retrieves all project_TechnologyTags from database
+	public List<Project_TechnologyTagDTO> getAllProject_TechnologyTags() throws SQLException
+	{
+		// List of all project_TechnologyTags in DTO form
+		List<Project_TechnologyTagDTO> allProject_TechnologyTags = new ArrayList<>();
+
+		// SQL Query statement
+		String QUERY = "SELECT * FROM PROJECTS_TECHNOLOGY_TAGS";
+
+		ResultSet rs = DatabaseWrapper.getQueryResult(QUERY);
+
+		Project_TechnologyTagMapper mapper = new Project_TechnologyTagMapper();
+
+		// Iterate through results and map database entries into DTO objects
+		while(rs.next())
+		{
+			Project_TechnologyTagDTO newProject_TechnologyTag = mapper.rowMap(rs);
+			allProject_TechnologyTags.add(newProject_TechnologyTag);
+		}
+
+		return allProject_TechnologyTags;
+	}
+
+	// Retrieves list of Project_TechnologyTags via ProjectID in DTO form
+	public List<Project_TechnologyTagDTO> getProject_TechnologyTagsByProjectID(int projectID) throws SQLException
+	{
+		// Result
+		List<Project_TechnologyTagDTO> allProject_TechnologyTags = new ArrayList<>();
+
+		// SQL Query statement
+		String QUERY = "SELECT * FROM PROJECTS_TECHNOLOGY_TAGS WHERE ProjectID=" + projectID;
+
+		ResultSet rs = DatabaseWrapper.getQueryResult(QUERY);
+
+		Project_TechnologyTagMapper mapper = new Project_TechnologyTagMapper();
+
+		// Iterate through results and map database entries into DTO objects
+		while(rs.next())
+		{
+			Project_TechnologyTagDTO newProject_TechnologyTag = mapper.rowMap(rs);
+			allProject_TechnologyTags.add(newProject_TechnologyTag);
+		}
+
+		return allProject_TechnologyTags;
+	}
+
+	// Retrieves list of Project_TechnologyTags via TechnologyTagID in DTO form
+	public List<Project_TechnologyTagDTO> getProject_TechnologyTagsByTechnologyTagID(int technologyTagID) throws SQLException
+	{
+		// Result
+		List<Project_TechnologyTagDTO> allProject_TechnologyTags = new ArrayList<>();
+
+		// SQL Query statement
+		String QUERY = "SELECT * FROM PROJECTS_TECHNOLOGY_TAGS WHERE TechnologyTagID=" + technologyTagID;
+
+		ResultSet rs = DatabaseWrapper.getQueryResult(QUERY);
+
+		Project_TechnologyTagMapper mapper = new Project_TechnologyTagMapper();
+
+		// Iterate through results and map database entries into DTO objects
+		while(rs.next())
+		{
+			Project_TechnologyTagDTO newProject_TechnologyTag = mapper.rowMap(rs);
+			allProject_TechnologyTags.add(newProject_TechnologyTag);
+		}
+
+		return allProject_TechnologyTags;
+	}
+
+	// Retrieves list of Project_TechnologyTags via ProjectID AND TechnologyTagID in DTO form
+	public List<Project_TechnologyTagDTO> getProject_TechnologyTagByBothID(int projectID, int technologyTagID) throws SQLException
+	{
+		// Result
+		List<Project_TechnologyTagDTO> allProject_TechnologyTags = new ArrayList<>();
+
+		// SQL Query statement
+		String QUERY = "SELECT * FROM PROJECTS_TECHNOLOGY_TAGS WHERE ProjectID=" + projectID + " AND TechnologyTagID=" + technologyTagID;
+
+		ResultSet rs = DatabaseWrapper.getQueryResult(QUERY);
+
+		Project_TechnologyTagMapper mapper = new Project_TechnologyTagMapper();
+
+		// Iterate through results and map database entries into DTO objects
+		while(rs.next())
+		{
+			Project_TechnologyTagDTO newProject_TechnologyTag = mapper.rowMap(rs);
+			allProject_TechnologyTags.add(newProject_TechnologyTag);
+		}
+
+		return allProject_TechnologyTags;
+	}
+
+	// Inserts project_TechnologyTag into database table
+	public Project_TechnologyTagDTO createProject_TechnologyTag (Project_TechnologyTagDTO project_TechnologyTag) throws SQLException
+	{
+
+		// SQL Query statement
+		String QUERY = "INSERT INTO PROJECTS_TECHNOLOGY_TAGS VALUES (";
+		QUERY += project_TechnologyTag.getProjectID() + ", ";
+		QUERY += project_TechnologyTag.getTechnologyID() + ")";
+
+		// Executes statement
+		ResultSet rs = DatabaseWrapper.getQueryResult(QUERY);
+
+		return project_TechnologyTag;
+	}
+
+	// No Update method needed for junction/association table
+
+	// Deletes project_TechnologyTag
+	public void deleteProject_TechnologyTag (Project_TechnologyTagDTO project_TechnologyTag) throws SQLException
+	{
+		int projectID = project_TechnologyTag.getProjectID();
+		int technologyTagID = project_TechnologyTag.getTechnologyID();
+
+		// SQL Query statement
+		String QUERY = "DELETE FROM PROJECTS_TECHNOLOGY_TAGS WHERE ProjectID=" + projectID + " AND TechnologyTagID=" + technologyTagID;
+
+		// Executes statement
+		ResultSet rs = DatabaseWrapper.getQueryResult(QUERY);
+	}
+}
