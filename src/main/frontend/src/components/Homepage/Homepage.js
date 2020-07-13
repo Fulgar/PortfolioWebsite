00507feb152e5aa5fc84fcdf9c9f3c82441921ae
Jason <jdawgerj5@gmail.com>
2020-07-12 22:39:32 -0400

    Modified build configuration

---
+++ src/main/frontend/src/components/Homepage/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Homepage.css';
+
+const Homepage = () => (
+  <div className="Homepage">
+    <b>HOMEPAGE</b>
+  </div>
+);
+
+Homepage.propTypes = {};
+
+Homepage.defaultProps = {};
+
+export default Homepage;
---
+++ src/main/frontend/src/components/Homepage/Homepage.stories.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Homepage from './Homepage';
+
+storiesOf('Homepage', module).add('default', () => <Homepage />);
