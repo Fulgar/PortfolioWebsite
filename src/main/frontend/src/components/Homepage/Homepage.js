b8f0787640f8d565fb48850304b8f4229324f723
Jason <jdawgerj5@gmail.com>
2020-07-23 18:40:13 -0400

    Set min width for side panel paper captions

--- src/main/frontend/src/components/Homepage/Homepage.js
+++ src/main/frontend/src/components/Homepage/Homepage.js
@@ -11,20 +11,20 @@ const Homepage = () => (
     <div className="Homepage">
         <div className={"main-content"}>
             <Paper variant={"outlined"} style={{
-                borderColor: "#CCA43B", borderWidth: 0.40 + "em", padding: 5 + "em"
+                borderColor: "#CCA43B", borderWidth: 0.40 + "em", padding: 3 + "em"
             }}>
                 <img className={"portrait-img"} src={portrait} alt={"Jason James"}/>
                 <Typography style={{paddingTop: 1 + "em", paddingBottom: 1 + "em"}} variant={"h3"}>
                     Jason James
                 </Typography>
 
-                <Paper style={{margin: 1 + "em", display: "flex", alignItems: "center"}} elevation={10}>
+                <Paper style={{margin: 1 + "em", display: "flex", alignItems: "center", minWidth: 150 + "px"}} elevation={10}>
                     <Typography variant={"subtitle1"}>
                         Career Status: Looking for Work
                     </Typography>
                 </Paper>
 
-                <Paper style={{margin: 1 + "em", display: "flex", alignItems: "center"}} elevation={10}>
+                <Paper style={{margin: 1 + "em", display: "flex", alignItems: "center", minWidth: 150 + "px"}} elevation={10}>
                     <img className={"social-media-logo"} src={githubLogo} alt={"Github Profile Page"}/>
                     <a href={"https://github.com/Fulgar"}>
                         <Typography variant={"subtitle1"}>
@@ -33,7 +33,7 @@ const Homepage = () => (
                     </a>
                 </Paper>
 
-                <Paper style={{margin: 1 + "em", display: "flex", alignItems: "center"}} elevation={10}>
+                <Paper style={{margin: 1 + "em", display: "flex", alignItems: "center", minWidth: 150 + "px"}} elevation={10}>
                     <img className={"social-media-logo"} src={linkedLogo} alt={"LinkedIn Profile Page"}/>
                     <a href={"https://www.linkedin.com/in/jason-james-011762146/"}>
                         <Typography variant={"subtitle1"}>
@@ -42,13 +42,13 @@ const Homepage = () => (
                     </a>
                 </Paper>
 
-                <Paper style={{margin: 1 + "em", display: "flex", alignItems: "center"}} elevation={10}>
+                <Paper style={{margin: 1 + "em", display: "flex", alignItems: "center", minWidth: 150 + "px"}} elevation={10}>
                     <Typography variant={"body2"}>
                         Professional Email: jjames541907@gmail.com
                     </Typography>
                 </Paper>
 
-                <Paper style={{margin: 1 + "em", display: "flex", alignItems: "center"}} elevation={10}>
+                <Paper style={{margin: 1 + "em", display: "flex", alignItems: "center", minWidth: 150 + "px"}} elevation={10}>
                     <Typography variant={"subtitle1"}>
                         Resume: Coming Soon
                     </Typography>
