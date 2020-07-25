b8e34bdbcfdec8b42b1d55a9557b2cfc819ba284
Jason <jdawgerj5@gmail.com>
2020-07-25 05:41:49 -0400

    Basic table mapping working

--- src/main/frontend/src/components/Projects/Projects.js
+++ src/main/frontend/src/components/Projects/Projects.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import './Projects.css';
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
@@ -8,41 +7,101 @@ import Table from "@material-ui/core/Table";
 import TableHead from "@material-ui/core/TableHead";
 import TableCell from "@material-ui/core/TableCell";
 import TableRow from "@material-ui/core/TableRow";
+import TableBody from "@material-ui/core/TableBody";
 
-const Projects = () => (
-  <div className="Projects">
-      <Typography style={{padding: 1 + "em"}} color={"secondary"} variant={"h3"}>
-          PROJECTS
-      </Typography>
+const Projects = () => {
+    const [data, setData] = useState([
+        {
+            "projectID": 1,
+            "title" : "Project A",
+            "description": "A university project",
+            "projectTypeID": 1,
+            "courseID": 1
+        },
+        {
+            "projectID": 2,
+            "title" : "Project B",
+            "description": "A personal project",
+            "projectTypeID": 2,
+            "courseID": null
+        },
+        {
+            "projectID": 3,
+            "title" : "Project C",
+            "description": "Another university project",
+            "projectTypeID": 1,
+            "courseID": 1
+        },
+        {
+            "projectID": 4,
+            "title" : "Project D",
+            "description": "Another personal project",
+            "projectTypeID": 2,
+            "courseID": null
+        }
+    ]);
 
-      <Paper className={ "about-paper"} style={{
-          borderColor: "#CCA43B", borderWidth: 0.40 + "em", padding: 5 + "em"}}
-             variant={"outlined"}>
-          <TableContainer>
-              <Table>
-                  <TableHead>
-                      <TableCell>Head1</TableCell>
-                      <TableCell>Head2</TableCell>
-                      <TableCell>Head3</TableCell>
-                  </TableHead>
-                  <TableRow>
-                      <TableCell>Hello</TableCell>
-                      <TableCell>There</TableCell>
-                      <TableCell>World</TableCell>
-                  </TableRow>
-                  <TableRow>
-                      <TableCell>How</TableCell>
-                      <TableCell>Are</TableCell>
-                      <TableCell>You</TableCell>
-                  </TableRow>
-              </Table>
-          </TableContainer>
-      </Paper>
-  </div>
-);
+    const [header, setHeader] = useState([
+        {
+            name: "Project ID",
+            prop: "projectID"
+        },
+        {
+            name: "Title",
+            prop: "title"
+        },
+        {
+            name: "Description",
+            prop: "description"
+        },
+        {
+            name: "Project Type ID",
+            prop: "projectTypeID"
+        },
+        {
+            name: "Course ID",
+            prop: "courseID"
+        }
+    ]);
 
-Projects.propTypes = {};
+    return (
+        <div className="Projects">
+            <Typography style={{padding: 1 + "em"}} color={"secondary"} variant={"h3"}>
+                PROJECTS
+            </Typography>
 
-Projects.defaultProps = {};
+            <Paper className={"about-paper"} style={{
+                borderColor: "#CCA43B", borderWidth: 0.40 + "em", padding: 5 + "em"}}
+                   variant={"outlined"}>
+                <TableContainer>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                {
+                                    header.map((head) => {
+                                        return <TableCell>{head.name}</TableCell>
+                                    })
+                                }
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                data.map((body) => {
+                                    return<TableRow>
+                                        <TableCell>{body.projectID}</TableCell>
+                                        <TableCell>{body.title}</TableCell>
+                                        <TableCell>{body.description}</TableCell>
+                                        <TableCell>{body.projectTypeID}</TableCell>
+                                        <TableCell>{body.courseID}</TableCell>
+                                    </TableRow>
+                                })
+                            }
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Paper>
+        </div>
+    );
+};
 
 export default Projects;
