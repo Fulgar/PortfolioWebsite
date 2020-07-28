6784a94b066713682de3bbeb795a3a2c5e5c6050
Jason <jdawgerj5@gmail.com>
2020-07-27 22:44:43 -0400

    ProjectPage now loads its own specific project database data with Title display and initialized Paper div element.

--- src/main/frontend/src/components/ProjectPage/ProjectPage.js
+++ src/main/frontend/src/components/ProjectPage/ProjectPage.js
@@ -1,18 +1,23 @@
 import React, {useEffect, useState} from 'react';
 
 import './ProjectPage.css';
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 
 const ProjectPage = (props) => {
     const projectID = props.projectID;
 
     // HTTP Request status
-    const [error, setError] = useState(null);
+    const [projectError, setProjectError] = useState(null);
     const [isProjectLoaded, setIsProjectLoaded] = useState(false);
+
+    // Container for all current Project data
+    const [projectData, setProjectData] = useState([]);
 
     // Is executed only on first render of Projects component
     useEffect(() => {
-        // Fetch all Project database data via GET request
-        fetch("/portfolio/project/getAll")
+        // Fetch specific Project database data via GET request
+        fetch("/portfolio/project/" + projectID)
             .then(res => res.json())
             .then(
                 (result) => {
@@ -21,14 +26,26 @@ const ProjectPage = (props) => {
                 },
                 (error) => {
                     console.error(error);
-                    setError(error);
+                    setProjectError(error);
                 }
             );
     }, []);
 
     return (
         <div className="ProjectPage">
-            ProjectPage Component
+            <Typography style={{padding: 1 + "em"}} color={"secondary"} variant={"h3"}>
+                {
+                    (isProjectLoaded === true)
+                        ? (<span>{projectData["title"]}</span>)
+                        : ((projectError) ? (<span>PROJECT NOT FOUND</span>) : (<span>PROJECT DATA LOADING</span>))
+                }
+            </Typography>
+
+            <Paper className={"about-paper"} style={{
+                borderColor: "#CCA43B", borderWidth: 0.40 + "em", padding: 5 + "em"}}
+                   variant={"outlined"}>
+
+            </Paper>
         </div>
     );
 };
