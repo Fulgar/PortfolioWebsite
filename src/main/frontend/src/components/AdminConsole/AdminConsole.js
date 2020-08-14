6c2045840e36ed71d6f20ef93c1a09da0c06d171
Jason <jdawgerj5@gmail.com>
2020-08-13 22:30:13 -0400

    Basic rudimentary create-contributor modal form successfully creates new contributor in database. Modal is not yet closed and AdminContributorTable is not yet updated upon submission of form.

--- src/main/frontend/src/components/AdminConsole/AdminConsole.js
+++ src/main/frontend/src/components/AdminConsole/AdminConsole.js
@@ -4,23 +4,44 @@ import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import AdminContributorTable from "../AdminContributorTable/AdminContributorTable";
 import Modal from "@material-ui/core/Modal";
+import AdminContributorAddModal from "../AdminContributorTable/AdminContributorAddModal/AdminContributorAddModal";
+import Button from "@material-ui/core/Button";
+import AddIcon from '@material-ui/icons/Add';
+
+const styles = {
+    addButtonInner: {
+        display: "flex", justifyContent: "center", alignItems: "center"
+    }
+};
 
 const AdminConsole = (props) => {
+    // Contributor Module fields
+    // Contributor "Add" Modal fields
+    const [contributorAddOpen, setContributorAddOpen] = useState(false);
+    const handleContributorAddOpen = () => {
+        setContributorAddOpen(true);
+    };
+    const handleContributorAddClose = () => {
+        setContributorAddOpen(false);
+    };
 
-  return (
+    return (
       <div className="AdminConsole">
           <Typography style={{padding: 1 + "em"}} color={"secondary"} variant={"h3"}>
               ADMIN PANEL
           </Typography>
 
           <Paper className={"contributor-view"} elevation={10}>
+              <Button variant={"contained"} color={"primary"} onClick={handleContributorAddOpen}>
+                  <span style={styles.addButtonInner}><AddIcon/> Add Contributor</span>
+              </Button>
               <AdminContributorTable/>
               <Modal
                   className={"admin-contributor-add-modal"}
                   open={contributorAddOpen}
                   onClose={handleContributorAddClose}
               >
-
+                  <AdminContributorAddModal/>
               </Modal>
           </Paper>
       </div>
--- src/main/frontend/src/components/AdminContributorTable/AdminContributorAddModal/AdminContributorAddModal.js
+++ src/main/frontend/src/components/AdminContributorTable/AdminContributorAddModal/AdminContributorAddModal.js
@@ -1,15 +1,96 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, {useState} from 'react';
 import './AdminContributorAddModal.css';
+import Paper from "@material-ui/core/Paper";
+import FormControl from "@material-ui/core/FormControl";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 
-const AdminContributorAddModal = () => (
-  <div className="AdminContributorAddModal">
-    AdminContributorAddModal Component
-  </div>
-);
+const styles = {
+    modalStyle: {
+        width: 750,
+        height: 750,
+        position: "absolute",
+        left: 50 + "%",
+        top: 50 + "%",
+        transform: "translate(-50%, -50%)",
+        display: "flex", justifyContent: "center", alignItems: "center"
+    }
+};
 
-AdminContributorAddModal.propTypes = {};
+const AdminContributorAddModal = () => {
+    // Form data
+    const [newFirstName, setNewFirstName] = useState("");
+    const [newLastName, setNewLastName] = useState("");
+    const [newGithubProfile, setNewGithubProfile] = useState("");
 
-AdminContributorAddModal.defaultProps = {};
+    const handleSubmit = async () => {
+        const data = {
+            "firstName": newFirstName,
+            "lastName": newLastName,
+            "githubProfileLink": newGithubProfile
+        };
+
+        const response = await fetch("/portfolio/contributor/create",
+            {
+                method: "POST",
+                mode: "cors",
+                cache: "no-cache",
+                credentials: "same-origin",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                redirect: "follow",
+                referrerPolicy: "no-referrer",
+                body: JSON.stringify(data)
+            });
+    };
+
+    return (
+        <Paper style={styles.modalStyle} elevation={10} className="AdminContributorAddModal">
+            <div className={"ContributorAddModalInner"}>
+                <Typography color={"primary"} variant={"h4"}>
+                    Create Contributor
+                </Typography>
+                <br/><br/><br/><br/>
+                <form autoComplete={"off"}>
+                    <TextField
+                        required
+                        id={"firstNameInput"}
+                        label={"First Name"}
+                        defaultValue={""}
+                        variant={"outlined"}
+                        onChange={(e) => {setNewFirstName(e.target.value)}}
+                    />
+                    <br/><br/>
+                    <TextField
+                        required
+                        id={"lastNameInput"}
+                        label={"Last Name"}
+                        defaultValue={""}
+                        variant={"outlined"}
+                        onChange={(e) => {setNewLastName(e.target.value)}}
+                    />
+                    <br/><br/>
+                    <TextField
+                        required
+                        id={"githubProfileInput"}
+                        label={"Github Profile Link"}
+                        defaultValue={""}
+                        variant={"outlined"}
+                        helperText={"MUST INCLUDE https://"}
+                        onChange={(e) => {setNewGithubProfile(e.target.value)}}
+                    />
+                    <br/><br/>
+                    <Button color={"primary"} variant={"contained"} onClick={() => {handleSubmit()}}>
+                       Submit
+                    </Button>
+                </form>
+            </div>
+
+        </Paper>
+    );
+};
+
 
 export default AdminContributorAddModal;
