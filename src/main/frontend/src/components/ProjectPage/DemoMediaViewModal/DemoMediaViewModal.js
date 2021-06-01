a5f84c52c95ce92615a65419ffd58ab4d9e2e778
Jason James <jdawgerj5@gmail.com>
2021-05-31 22:26:04 -0400

    PDF is now supported across all devices

--- src/main/frontend/src/components/ProjectPage/DemoMediaViewModal/DemoMediaViewModal.js
+++ src/main/frontend/src/components/ProjectPage/DemoMediaViewModal/DemoMediaViewModal.js
@@ -2,6 +2,9 @@ import React, {useState} from 'react';
 import './DemoMediaViewModal.css';
 import Typography from "@material-ui/core/Typography";
 import {ArrowBack, ArrowForward, Close} from "@material-ui/icons";
+// import {Document, Page, pdfjs} from 'react-pdf';
+import { Document, Page, pdfjs } from 'react-pdf/dist/esm/entry.webpack';
+
 
 const DemoMediaViewModal = (props) => {
 
@@ -14,6 +17,9 @@ const DemoMediaViewModal = (props) => {
 
 	// Contains the index of the currently viewed demoMedia file
 	const [demoMediaIndex, setDemoMediaIndex] = useState(0);
+
+	// Contains the total number of pages of the currently loaded PDF (If loaded)
+	const [numPages, setNumPages] = useState(null);
 
 
 	// Method for returning DemoMedia of IMG type
@@ -31,11 +37,12 @@ const DemoMediaViewModal = (props) => {
 	};
 
 	// Method for returning DemoMedia of TXT type
+	// TODO: Need to read data from TXT files and display that data, instead of iframe
 	const getDemoText = (url) => {
-		return (<p>
+		return (<p className={"demo-text-p"}>
 				<iframe className={"demo-media-content"} src={url} style={{
 					frameBorder: 0,
-					minWidth: 100 + "%", minHeight: 50 + "vh"
+					minHeight: 50 + "vh"
 				}}>
 					Browser not compatible
 				</iframe>
@@ -43,15 +50,29 @@ const DemoMediaViewModal = (props) => {
 		);
 	};
 
+	const onPDFLoad = ({numPages}) => {
+		setNumPages(numPages);
+	};
+
 	// Method for returning DemoMedia of PDF type
 	const getDemoPDF = (url) => {
-		return (<iframe className={"demo-media-content"} src={url} style={{
-				frameBorder: 0,
-				minWidth: 100 + "%", minHeight: 80 + "vh"
-			}}>
-				Browser not compatible
-			</iframe>
+		console.log(url);
+		return (
+			<div className={"demo-media-content"} style={{overflowY: "scroll"}}>
+				<Document file={url} onLoadError={console.error} onLoadSuccess={onPDFLoad}>
+					{
+						Array.apply(null, Array(numPages)).map((x, i)=>i+1).map(page => <Page pageNumber={page}/>)
+					}
+				</Document>
+			</div>
 		);
+		// return (<iframe className={"demo-media-content"} src={url} style={{
+		// 		frameBorder: 0,
+		// 		minWidth: 100 + "%", minHeight: 80 + "vh"
+		// 	}}>
+		// 		Browser not compatible
+		// 	</iframe>
+		// );
 	};
 
 	// Method for returning appropriate DemoMedia content
@@ -96,8 +117,7 @@ const DemoMediaViewModal = (props) => {
 		let result = "left-arrow ";
 		if (doesLeftMediaExist()) {
 			result += "arrow-more-content";
-		}
-		else {
+		} else {
 			result += "arrow-no-content";
 		}
 		return result;
@@ -108,8 +128,7 @@ const DemoMediaViewModal = (props) => {
 		let result = "right-arrow ";
 		if (doesRightMediaExist()) {
 			result += "arrow-more-content";
-		}
-		else {
+		} else {
 			result += "arrow-no-content";
 		}
 		return result;
@@ -129,22 +148,26 @@ const DemoMediaViewModal = (props) => {
 		}
 	};
 
-	console.log(props.demoMediaData);
 	return (
 		<div className="DemoMediaViewModal">
 			<div className={"media-title"}>
-				<Typography variant={"h3"} color={"secondary"}>{props.demoMediaData[demoMediaIndex].mediaTitle}</Typography>
+				<Typography variant={"h3"}
+				            color={"secondary"}>{props.demoMediaData[demoMediaIndex].mediaTitle}</Typography>
 			</div>
 			{
 				getDemoContent()
 			}
 			<div className={"media-description"}>
-				<Typography variant={"body1"} color={"secondary"}>{props.demoMediaData[demoMediaIndex].mediaCaption}</Typography>
+				<Typography variant={"body1"}
+				            color={"secondary"}>{props.demoMediaData[demoMediaIndex].mediaCaption}</Typography>
 			</div>
 
-			<Close className={"close-modal-button"} style={styles.modalButton} color={"secondary"} onClick={props.handleDemoMediaViewClose}/>
-			<ArrowBack className={getLeftButtonClasses()} style={styles.modalButton} color={"secondary"} onClick={handleLeftButton}/>
-			<ArrowForward className={getRightButtonClasses()} style={styles.modalButton} color={"secondary"} onClick={handleRightButton}/>
+			<Close className={"close-modal-button"} style={styles.modalButton} color={"secondary"}
+			       onClick={props.handleDemoMediaViewClose}/>
+			<ArrowBack className={getLeftButtonClasses()} style={styles.modalButton} color={"secondary"}
+			           onClick={handleLeftButton}/>
+			<ArrowForward className={getRightButtonClasses()} style={styles.modalButton} color={"secondary"}
+			              onClick={handleRightButton}/>
 		</div>
 	)
 };
